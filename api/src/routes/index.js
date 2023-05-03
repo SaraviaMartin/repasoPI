5bd4fc69e2cb1c111669a635c079c9186c7797e6
Juan Saravia <saraviamartin16@hotmail.com>
2023-05-03 19:49:55 -0300

    Router config

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const userRouter = require("../routes/usersRouter");
+const postsRouter = require("../routes/postsRouter");
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
 
 
 module.exports = router;
